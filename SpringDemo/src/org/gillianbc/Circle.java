5bae804ab50637412004828095f2982dd62c0d48
GROUP\Gillian.Bladen-Clark <gillian.bladen-clark@olmgroup.com>
2018-06-18 22:12:15 +0100

    Added Required annotation and a BeanPostProcessor to process it

--- SpringDemo/src/org/gillianbc/Circle.java
+++ SpringDemo/src/org/gillianbc/Circle.java
@@ -1,11 +1,14 @@
 package org.gillianbc;
 
+import org.springframework.beans.factory.annotation.Required;
+
 public class Circle implements Shape{
 	private Point centre;
 	
 	public Point getCentre() {
 		return centre;
 	}
+	@Required
 	public void setCentre(Point centre) {
 		this.centre = centre;
 	}
