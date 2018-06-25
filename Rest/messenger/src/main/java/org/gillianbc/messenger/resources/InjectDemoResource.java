68114d67559f25f2d72241d91a6df9fa65bc6122
GROUP\Gillian.Bladen-Clark <gillian.bladen-clark@olmgroup.com>
2018-06-25 12:09:31 +0100

    REST Web Services - Tut 24 more added to InjectDemo

--- Rest/messenger/src/main/java/org/gillianbc/messenger/resources/InjectDemoResource.java
+++ Rest/messenger/src/main/java/org/gillianbc/messenger/resources/InjectDemoResource.java
@@ -9,55 +9,75 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/injectdemo")
 @Consumes(MediaType.TEXT_PLAIN)
 @Produces(MediaType.TEXT_PLAIN)
 public class InjectDemoResource {
 	/**
-	 * You can use any use any combination of these params
-	 * There are others too.
+	 * You can use any use any combination of these params There are others too.
 	 */
-	
-	
-	//e.g. http://localhost:8080/messenger/webapi/injectdemo/frogface
-	//Postman shows the available cookies on a separate tab - not sure how they got there
+
+	// Example endpoint -
+	// http://localhost:8080/messenger/webapi/injectdemo/fish/crab
+	@GET
+	@Path("/{level1}/{level2}")
+	public String getParamsUsingContext(@Context UriInfo uriInfo) {
+		// uriInfo has loads of useful get methods
+		MultivaluedMap<String, String> map = uriInfo.getPathParameters();
+		System.out.println(map);
+		System.out.println(map.get("level1"));
+		return "Method getParamsUsingContext ran using path " + uriInfo.getAbsolutePath() + "\n Level 1 "
+				+ map.get("level1") + "\n Level 2 "
+						+ map.get("level2");
+	}
+
+	// Example endpoint - http://localhost:8080/messenger/webapi/injectdemo/frogface
+	// Postman shows the available cookies on a separate tab - not sure how they got
+	// there
 	@GET
 	@Path("frogface")
 	public String getParamsUsingAnnotations(@CookieParam("JSESSIONID") String cookieParam) {
 		return "Method getParamsUsingAnnotations ran " + cookieParam;
 	}
-	
-	/*
-	//e.g. http://localhost:8080/messenger/webapi/injectdemo/frogface
-	//plus a Header of myCustomParam with a value of anything e.g. a session id
-	@GET
-	@Path("frogface")
-	public String getParamsUsingAnnotations(@HeaderParam("myCustomParam") String headerParam) {
-		return "Method getParamsUsingAnnotations ran " + headerParam;
-	}
-	*/
-	
-	
-	/*
-	// e.g. http://localhost:8080/messenger/webapi/injectdemo/frogface;param=fishhead
-	//Note it's a semicolon not a colon
-	@GET
-	@Path("frogface")
-	public String getParamsUsingAnnotations(@MatrixParam("param") String matrixParam) {
-		return "Method getParamsUsingAnnotations ran " + matrixParam;
-	}
-	*/
 
 	/*
-	// e.g. http://localhost:8080/messenger/webapi/injectdemo/frogface
-	// Make sure there's no header info in the Postman HTTP request
-	@GET
-	@Path("frogface")
-	public String getParamsUsingAnnotations() {
-		return "Method getParamsUsingAnnotations ran";
-	}
-	*/
+	 * //Example endpoint -
+	 * http://localhost:8080/messenger/webapi/injectdemo/frogface //plus a Header of
+	 * myCustomParam with a value of anything e.g. a session id
+	 * 
+	 * @GET
+	 * 
+	 * @Path("frogface") public String
+	 * getParamsUsingAnnotations(@HeaderParam("myCustomParam") String headerParam) {
+	 * return "Method getParamsUsingAnnotations ran " + headerParam; }
+	 */
+
+	/*
+	 * // Example endpoint -
+	 * http://localhost:8080/messenger/webapi/injectdemo/frogface;param=fishhead
+	 * //Note it's a semicolon not a colon
+	 * 
+	 * @GET
+	 * 
+	 * @Path("frogface") public String
+	 * getParamsUsingAnnotations(@MatrixParam("param") String matrixParam) { return
+	 * "Method getParamsUsingAnnotations ran " + matrixParam; }
+	 */
+
+	/*
+	 * // Example endpoint -
+	 * http://localhost:8080/messenger/webapi/injectdemo/frogface // Make sure
+	 * there's no header info in the Postman HTTP request
+	 * 
+	 * @GET
+	 * 
+	 * @Path("frogface") public String getParamsUsingAnnotations() { return
+	 * "Method getParamsUsingAnnotations ran"; }
+	 */
 
 }
